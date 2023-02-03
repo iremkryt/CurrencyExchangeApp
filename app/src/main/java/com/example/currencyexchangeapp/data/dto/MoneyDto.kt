59a35edc8afe1cc75d122194b040530a6c9d3c92
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-02-03 21:36:26 +0300

    Changed variables name with serializedname

--- app/src/main/java/com/example/currencyexchangeapp/data/dto/MoneyDto.kt
+++ app/src/main/java/com/example/currencyexchangeapp/data/dto/MoneyDto.kt
@@ -1,19 +1,24 @@
 package com.example.currencyexchangeapp.data.dto
 
 import com.example.currencyexchangeapp.domain.model.Money
+import com.google.gson.annotations.SerializedName
 
 data class MoneyDto(
-    val Alış: String,
-    val Değişim: String,
-    val Satış: String,
-    val Tür: String
+    @SerializedName("alis")
+    val alis: String?,
+    @SerializedName("degisim")
+    val degisim: String,
+    @SerializedName("satis")
+    val satis: String,
+    @SerializedName("tur")
+    val tur: String
 )
 
-fun MoneyDto.toMoney(): Money {
-    return Money(
-        Alış = Alış,
-        Değişim = Değişim,
-        Satış = Satış,
-        Tür = Tür
-    )
-}
+//fun MoneyDto.toMoney(): Money {
+//    return Money(
+//        Alış = Alış,
+//        Değişim = Değişim,
+//        Satış = Satış,
+//        Tür = Tür
+//    )
+//}
